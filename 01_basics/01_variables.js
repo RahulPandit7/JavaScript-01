85bbeb84df8f77e339e0e936c18ad2a556a593f5
Rahul Pandit <146408900+RahulPandit7@users.noreply.github.com>
2024-07-03 09:03:00 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1453
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "241678"
+accountCity = "kalaiya"
+let accountState;   // undefind 
+
+
+
+// accountId = 2  // not allowed
+
+accountEmail = "pandit@gmai.com"
+accountPassword = "2re6736e8"
+accountCity = "ktm"   // prefer to not use 
+console.log(accountId);  
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3)  // we are useing nodejs, not browser
+
+
+console.log(3 + 3);
+
+
+let name = "Rahul"  //string 
+let age = 18 //number
+let isLoggedIn = true //boolean
+let state;  //undefined
+let city = null //standalone 
+
+
+
+// number => range 2 to power 53 
+// bigint => more then 2 to power 53 range 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+
+// object
+
+
+console.log(typeof undefined);   //undefined
+console.log(typeof null);  //object
