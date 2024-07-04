4f6e33f230ba881a5f3a6d204cde4aee99387c7b
Rahul Pandit <146408900+RahulPandit7@users.noreply.github.com>
2024-07-04 08:39:17 +0000

    Conversion Operation and data types summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,18 +3,18 @@
 let Scroe = "33abc"
 let Range = "33" // string 
 
-console.log(typeof Scroe);  // choose any one to check a data type both are same 
-console.log(typeof(Scroe));
+// console.log(typeof Scroe);  // choose any one to check a data type both are same 
+// console.log(typeof(Scroe));
 
-console.log(typeof Range);
-console.log(typeof(Range));
+// console.log(typeof Range);
+// console.log(typeof(Range));
 
 
 //Number
 
 let valueInNumber = Number(Scroe)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // value     =>     output
@@ -29,7 +29,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); // output true 
+// console.log(booleanIsLoggedIn); // output true 
 
 // 1 => true;  0 => false 
 // "" => false 
@@ -39,5 +39,48 @@ console.log(booleanIsLoggedIn); // output true
  let someNumber = 33
 
  let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+// ************** Operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// string ADD
+
+let str1 = "hello"
+let str2 = " Rahul"
+
+let str3 = str1 + str2
+// console.log(str3);    // hello rahul
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2);  // 122
+// console.log(1 + 2 + 2);  // 32
+// console.log(3 + 4 * 5 % 3);  // 5
+
+// console.log(true);  // true
+// console.log(+true); // 1
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2    // 4 all 
+
+
+let gameCounter = 100
+// gameCounter++;  // 101
+++gameCounter; // 101
+// console.log(gameCounter);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+//  Primitive data types 
+
+// 7 types 
+
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const Scroe = 100
+const numScroe = 100.32
+
+const isLoggedIn = false 
+const outSideTemp = null 
+let userEmail;
+const id = Symbol("123")
+
+const BigIntNum = 6468441875415741n
+
+
+// Reference (Non Primitive)
+
+// Array, objects, Functions
+
+const heros = ["rahul", "aarti", "pandit"]
+console.log(typeof heros);
+
+let myobj = {
+    name: "Rahul",
+    age: 22,
+}
+
+console.log(typeof myobj);
+
+
+const myFunction = function () {
+    console.log("hello");
+}
+
+console.log(typeof myFunction);
+
+
